8f78e9d76c2c722efde628e784d52004a94929f9
Shubhkarman Singh Rathore <shubhkarman4733@yahoo.in>
2023-07-30 10:23:48 -0700

    adds lab

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES
+    ("Mr. Chocolate", 3, "M", "white", "HIGH", 1),
+    ("Rowdy", 4, "F", "black", "MED", 0),
+    ("Tabitha", 5, "M", "white", "LOW", 1),
+    ("Sergeant Brown", 7, "F", "white", "HIGH", 1),
+    ("Melissa", 3, "F", "black", "LOW", 0),
+    ("Grinch", 10, "M", "white", "HIGH", 1),
+    ("Wendy", 13, "F", "black", "MED", 1),
+    (NULL, 10, "M", "skin", "HIGH", 0);
